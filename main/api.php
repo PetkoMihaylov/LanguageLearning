16583d180a15de19c1d7a8152cb333b6b28146c8
PetkoMihaylov <pet99@mail.bg>
2018-01-06 21:07:51 +0200

    Add database connection.

---
+++ main/api.php
@@ -0,0 +1,34 @@
+<?php
+//header('Content-Type: application/json');
+
+function incrementScore($db)
+{
+	$result = $db->query("SELECT points FROM players WHERE id = 1");
+	$score = $result->fetch_assoc()["points"];
+	$score += 1;
+	$db->query("UPDATE players SET points = $score WHERE id = 1");
+}
+
+$db = new mysqli("localhost", "root", "root", "words");
+
+$result = $db->query("SELECT * FROM players");
+if(!$result)
+{
+	print($db->error);
+}
+
+$data = [];
+while($values = $result->fetch_assoc())
+{
+	array_push($data, $values);
+}
+
+//print (json_encode($data, JSON_UNESCAPED_UNICODE));
+$command = file_get_contents ("php://input");
+if($command == "increment-score")
+{
+	incrementScore($db);
+}
+
+$db->close();
+?>
